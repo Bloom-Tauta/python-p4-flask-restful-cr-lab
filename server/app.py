506d1dd54ab1915d87bb710370b8e094423dee11
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2025-04-09 11:47:14 +0300

    done

--- server/app.py
+++ server/app.py
@@ -17,11 +17,35 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants_dict_list = [plant.to_dict() for plant in Plant.query.all()]
+        response = make_response(jsonify(plants_dict_list), 200)
+        return response
+    
+    def post(self):
+        data = request.get_json()
+        plant = Plant(
+            name = data['name'],
+            image = data['image'],
+            price = data['price'],
+        )
+
+        db.session.add(plant)
+        db.session.commit()
+
+        plant_dict = plant.to_dict()
+        response = make_response(jsonify(plant_dict), 201)
+        return response
+
+api.add_resource(Plants, "/plants")
 
 class PlantByID(Resource):
-    pass
-        
-
+    def get(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        plant_dict = plant.to_dict()
+        response = make_response(jsonify(plant_dict), 200)
+        return response
+      
+api.add_resource(PlantByID, '/plants/<int:id>')
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -7,3 +7,11 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    image = db.Column(db.String)
+    price = db.Column(db.Float)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    edited_at = db.Column(db.DateTime, onupdate=db.func.now())
+    
+    def __repr__(self):
+        return f'<Newsletter {self.name}, published at {self.created_at}.>'
